23f93581b587dfedcd6fd2e473e09abbb58c188a
Luciano's tech <38385411+PIBSAS@users.noreply.github.com>
2025-03-25 10:23:09 -0300

    aa

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 const $Consultas = "https://campusvirtual.unm.edu.ar/moodle/mod/forum/view.php?id=365957";
 
-document.querySelectorAll(".con").forEach(link => {
+document.getElementsByClassName("con").forEach(link => {
     link.href = $Consultas;
 });
