24837f154cfb3ca2dcfa7fc8aced345f3d5ed5c3
Luciano's tech <38385411+PIBSAS@users.noreply.github.com>
2025-03-25 19:47:31 -0300

    libro 3 cuatri 1

--- js/script.js
+++ js/script.js
@@ -1,2 +1,4 @@
 const $Consultas = "https://campusvirtual.unm.edu.ar/moodle/mod/forum/view.php?id=365957";
 document.getElementsByClassName('con')[0].href = $Consultas;
+const $GeoGebra = "https://www.geogebra.org/classic?lang=es";
+document.getElementsByClassName('Geo')[0].href = $GeoGebra;
