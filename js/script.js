e1e517703090d6575ae2a2bb99f3348624490f9c
Luciano's tech <38385411+PIBSAS@users.noreply.github.com>
2025-03-25 21:29:20 -0300

    l

--- js/script.js
+++ js/script.js
@@ -2,5 +2,5 @@ const $Consultas = "https://campusvirtual.unm.edu.ar/moodle/mod/forum/view.php?i
 document.getElementsByClassName('con')[0].href = $Consultas;
 const $GeoGebra = "https://www.geogebra.org/classic?lang=es";
 document.getElementsByClassName('Geo')[0].href = $GeoGebra;
-const $Requisitos = "";
+const $Requisitos = "https://drive.google.com/file/d/1OjLunEFZ8VwrUmWhlOEmdlTYVoPUjepR/view?usp=sharing";
 document.getElementsByClassName('Req')[0].href = $Requisitos;
