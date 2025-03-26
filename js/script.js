7ef4bb3890c4d117a805338d330ce4049c672878
Luciano's tech <38385411+PIBSAS@users.noreply.github.com>
2025-03-25 21:09:43 -0300

    Libro 3 fin

--- js/script.js
+++ js/script.js
@@ -2,3 +2,5 @@ const $Consultas = "https://campusvirtual.unm.edu.ar/moodle/mod/forum/view.php?i
 document.getElementsByClassName('con')[0].href = $Consultas;
 const $GeoGebra = "https://www.geogebra.org/classic?lang=es";
 document.getElementsByClassName('Geo')[0].href = $GeoGebra;
+const $Requisitos = "";
+document.getElementsByClassName('Req')[0].href = $Requisitos;
