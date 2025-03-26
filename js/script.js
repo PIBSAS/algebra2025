430bd3207eb6d94e4da313d1f5cb8fac0ac4e3e8
Luciano's tech <38385411+PIBSAS@users.noreply.github.com>
2025-03-25 21:59:26 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
-const $Consultas = "https://campusvirtual.unm.edu.ar/moodle/mod/forum/view.php?id=365957";
-document.getElementsByClassName('con')[0].href = $Consultas;
-const $GeoGebra = "https://www.geogebra.org/classic?lang=es";
-document.getElementsByClassName('Geo')[0].href = $GeoGebra;
-const $Requisitos = "https://drive.google.com/file/d/1OjLunEFZ8VwrUmWhlOEmdlTYVoPUjepR/view?usp=sharing";
-document.getElementsByClassName('Req')[0].href = $Requisitos;
+document.addEventListener("DOMContentLoaded", function() {
+    const $Consultas = "https://campusvirtual.unm.edu.ar/moodle/mod/forum/view.php?id=365957";
+    document.getElementsByClassName('con')[0].href = $Consultas;
+
+    const $GeoGebra = "https://www.geogebra.org/classic?lang=es";
+    document.getElementsByClassName('Geo')[0].href = $GeoGebra;
+
+    const $Requisitos = "https://drive.google.com/file/d/1OjLunEFZ8VwrUmWhlOEmdlTYVoPUjepR/view?usp=sharing";
+    document.getElementsByClassName('Req')[0].href = $Requisitos;
+});
